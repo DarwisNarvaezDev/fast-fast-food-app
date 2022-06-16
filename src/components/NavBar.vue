421bd7ce793fb3df0f4262fbca1ec5008da33b74
siwrad <dnarvaez@ad-cap.com.ar>
2022-06-15 23:28:09 -0300

    pellets fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -122,13 +122,25 @@ export default {
         distributePellets() {
             const sessionObject = callSessionObject();
             const obj = JSON.parse(sessionObject);
-            if (obj.burgers || obj.salads || obj.drinks) {
-                const jsonObj = JSON.parse(sessionObject)
-                const { burgers, salads, drinks } = jsonObj;
-                this.burgerItems = this.populateQuantities(burgers);
-                this.saladItems = this.populateQuantities(salads);
-                this.drinkItems = this.populateQuantities(drinks);
+            let burgerQty = 0;
+            let saladQty = 0;
+            let drinkQty = 0;
+            if (obj) {
+                obj.forEach(element => {
+                    if (element.menuType === 1) {
+                        burgerQty += element.qty;
+                    }
+                    if (element.menuType === 2) {
+                        saladQty += element.qty;
+                    }
+                    if (element.menuType === 3) {
+                        drinkQty += element.qty;
+                    }
+                })
             }
+            this.burgerItems = burgerQty;
+            this.saladItems = saladQty;
+            this.drinkItems = drinkQty;
         },
         populateQuantities(array) {
             let qty = 0;
